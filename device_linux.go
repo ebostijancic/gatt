fc33cb9d31551f58dec1aa8c4a21dbfc67bd322b
Emil Bostijancic <emil.bostijancic@gmail.com>
2017-07-11 14:05:36 +0200

    passive and shorter interval scan window

--- device_linux.go
+++ device_linux.go
@@ -45,9 +45,9 @@ func NewDevice(opts ...Option) (Device, error) {
 			AdvertisingFilterPolicy: 0x00,
 		},
 		scanParam: &cmd.LESetScanParameters{
-			LEScanType:           0x01,   // [0x00]: passive, 0x01: active
-			LEScanInterval:       0x0010, // [0x10]: 0.625ms * 16
-			LEScanWindow:         0x0010, // [0x10]: 0.625ms * 16
+			LEScanType:           0x00,   // [0x00]: passive, 0x01: active
+			LEScanInterval:       0x0004, // [0x10]: 0.625ms * 16
+			LEScanWindow:         0x0004, // [0x10]: 0.625ms * 16
 			OwnAddressType:       0x00,   // [0x00]: public, 0x01: random
 			ScanningFilterPolicy: 0x00,   // [0x00]: accept all, 0x01: ignore non-white-listed.
 		},
